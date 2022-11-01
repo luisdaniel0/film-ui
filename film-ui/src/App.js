c9651bfb53fb3c79a5fca556d31b2ff891fab6c8
Luis Sanchez <luisanchezdh@gmail.com>
2022-11-01 14:33:23 -0400

    fetched api, useEffect, useState, works. Adding FilmGrid

---
+++ film-ui/src/App.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import FilmGrid from "./components/FilmGrid"
+
+
+
+function App() {
+  const [films, setFilms] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchFilms = async () => {
+      try {
+        const result = await axios.get(`https://restapi-production-9aa5.up.railway.app/films`)
+        setFilms(result.data)
+        setIsLoading(false)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchFilms();
+  }, [])
+
+  console.log(films)
+  return (
+    <div className="container">
+      <FilmGrid />
+
+    </div>
+  );
+}
+
+export default App;
